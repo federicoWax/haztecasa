431a2522ee09a46557aad8f5f52df19b37f170d1
fedeWax <cha_lino_23@hotmail.com>
2025-04-23 22:08:27 -0700

    Test

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       designSize: const Size(360, 690),
       minTextAdapt: true,
       splitScreenMode: true,
-      builder: (context, child) {
+      builder: (_, child) {
         return MaterialApp(debugShowCheckedModeBanner: false, home: child);
       },
       child: const MapPage(),
